f1721d461773c26643c971d0204506dec5f87f28
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-08-09 15:00:47 +0100

    code now connects to ubnt mpower devices and reads power info

--- ubntmpowerreader/Program.cs
+++ ubntmpowerreader/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,36 @@ namespace ubntmpowerreader
     {
         static void Main(string[] args)
         {
+            string servers = ConfigurationManager.AppSettings["servers"];
+            string username = ConfigurationManager.AppSettings["username"];
+            string password = ConfigurationManager.AppSettings["password"];
+            foreach (string server in servers.Split(','))
+            {
+                ConnectionInfo ConnNfo = new ConnectionInfo(server, 22, username,
+                new AuthenticationMethod[]{
+                // Pasword based Authentication
+                new PasswordAuthenticationMethod(username,password)
+                }
+                );
+                string[] items = { "active_pwr", "energy_sum" };
+
+                using (var sshclient = new SshClient(ConnNfo))
+                {
+                    Console.WriteLine("Connecting to {0}", server);
+                    sshclient.Connect();
+                    // quick way to use ist, but not best practice - SshCommand is not Disposed, ExitStatus not checked...
+                    foreach (string s in items)
+                    {
+                        for (int i = 1; i < 7; i++)
+                        {
+                            Console.WriteLine(string.Format("{0}{1}", s, i));
+                            Console.WriteLine(sshclient.CreateCommand(string.Format("cd /proc/power && cat {0}{1}", s, i)).Execute());
+                        }
+                    }
+                    sshclient.Disconnect();
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
